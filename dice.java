de5ce98f81b98900b302374722ce71cf809ee29b
이재현201611095 <jhlsuper@gmail.com>
2016-10-10 12:18:54 +0900

    Add files via upload

---
+++ dice.java
@@ -0,0 +1,12 @@
+﻿for(int i=0; i<10;i++)
+    d.roll();
+class dice {
+    private int number = 0;
+    public void roll() {
+        number = (int) (Math.random() * 6);
+        System.out.println("Turn on " + number);
+    }
+    public int getNum() {
+        return this.number;
+    }
+}
---
+++ player.java
@@ -0,0 +1,21 @@
+﻿public class Player {
+    private String name=null;
+    int num=0;
+    public Player(String _name) {
+        name=_name;
+    }
+    public void play() {
+        dice d=new dice();
+        //num=d.roll();
+        d.roll();
+        num=d.getNum();
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getNum() {
+        return this.num;
+    }
+}
+Player p=new Player("jh");
+p.play();
