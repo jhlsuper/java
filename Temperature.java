e55ee28263fcfeb1cf6e15011382cbedd85951ac
이재현201611095 <jhlsuper@gmail.com>
2016-11-02 13:15:33 +0900

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,19 @@
+public class Temperature{
+	public void change(float temp,String sel){
+		float t;
+		if (sel.equals("'F")){
+			t=(temp-32f)/1.8f;
+			System.out.println(temp+sel+"->"+t+"'C");
+}
+      else if(sel.equals("'C")){
+			t=(temp*1.8f)+32;
+			System.out.println(temp+sel+"->"+t+"'F");
+}
+
+}
+	public static void main(String[] args){
+		Temperature t=new Temperature();
+		t.change(108,"'F");
+		t.change(28,"'C");
+}
+}
