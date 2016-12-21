8dee42d8504798676661cd84aa574f6cd945c905
이재현201611095 <jhlsuper@gmail.com>
2016-12-21 18:32:16 +0900

    Update DrawTriangle.java
    수정하였습니다

--- DrawTriangle.java
+++ DrawTriangle.java
@@ -2,11 +2,14 @@ class DrawTriangle {
 
 public static void maketriangle() {
 String t="*";
+String e=" ";
 StringBuffer sbuf=new StringBuffer();
-for(int i=0;i<5;i++) {
-    for(int j=0;j<i;j++) {
-        sbuf.append(t);
+for(int i=0;i<6;i++) {
+    for(int j=0;j<(6-i);j++) {
+        sbuf.append(e);
     }
+    for(int j=0;j<(i*2)-1;j++){
+        sbuf.append(t);
     System.out.printf("%d %s\n",i,sbuf.toString());
     sbuf.delete(0, sbuf.length());
 }
