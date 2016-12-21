454cadb48525f5ffafdac584536026f4941faf72
이재현201611095 <jhlsuper@gmail.com>
2016-12-21 19:53:20 +0900

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,12 +1,18 @@
+package com.sd.ch8;
 public class Circle extends Point { 
+ private double getArea;
+ private double radius;
+ public Circle(int x, int y, int radius) {
  super(x,y); 
- double area; 
- private double getArea(double radius) { 
- area=radius*radius*(3.14); 
- System.out.println(x); 
- System.out.println(y); 
- System.out.println(area); 
- 
+ this.radius = radius;
+ }
+ public double getArea() { 
+ return getArea=radius*radius*Math.PI;
+ }
+ public static void main(String[] args) {
+  Circle c = new Circle(20,20,10);
+  double myArea=c.getArea();
+  System.out.println(myArea);
  
  } 
  } 
